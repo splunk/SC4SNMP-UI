bf463e8eb6b3bfa5f26a5083408148e2436e3752
Wojciech Zyla <wzya@splunk.com>
2022-09-22 13:17:30 +0200

    fix: editing and deleting inventory records - backend

--- backend/app.py
+++ backend/app.py
@@ -10,29 +10,43 @@ db = client.sc4snmp
 
 
 @app.route('/')
-@cross_origin(origin='*', headers=['access-control-allow-origin', 'Content-Type'])
+@cross_origin(origins='*', headers=['access-control-allow-origin', 'Content-Type'])
 def hello_world():
     return 'Hello World!'
 
 
 @app.route('/inventory')
-@cross_origin(origin='*', headers=['access-control-allow-origin', 'Content-Type'])
+@cross_origin(origins='*', headers=['access-control-allow-origin', 'Content-Type'])
 def get_inventory_list():
     inventory = db.inventory.find()
     return json_util.dumps(list(inventory))
 
 
 @app.route('/inventory/add', methods=['POST'])
-@cross_origin(origin='*', headers=['access-control-allow-origin', 'Content-Type'])
+@cross_origin(origins='*', headers=['access-control-allow-origin', 'Content-Type'])
 def add_inventory_record():
     inventory_obj = request.json
     print(inventory_obj)
     db.inventory.insert_one(inventory_obj)
     return "success"
 
+@app.route('/inventory/delete/<inventory_id>', methods=['POST'])
+@cross_origin(origins='*', headers=['access-control-allow-origin', 'Content-Type'])
+def delete_inventory_record(inventory_id):
+    db.inventory.delete_one({'_id': ObjectId(inventory_id)})
+    return "success"
+
+@app.route('/inventory/update/<inventory_id>', methods=['POST'])
+@cross_origin(origins='*', headers=['access-control-allow-origin', 'Content-Type'])
+def update_inventory_record(inventory_id):
+    inventory_obj = request.json
+    print(f"{inventory_obj}")
+    new_values = {"$set": inventory_obj}
+    db.inventory.update_one({'_id': ObjectId(inventory_id)}, new_values)
+    return "success"
 
 @app.route('/profiles')
-@cross_origin(origin='*', headers=['access-control-allow-origin', 'Content-Type'])
+@cross_origin(origins='*', headers=['access-control-allow-origin', 'Content-Type'])
 def get_profiles_list():
     profiles = db.profiles.find()
     profiles_list = list(profiles)
@@ -50,7 +64,7 @@ def get_all_profiles_list():
 
 #@cross_origin(origin='*', headers=['access-control-allow-origin', 'Content-Type'])
 @app.route('/profiles/add', methods=['POST'])
-@cross_origin(origin='*', headers=['access-control-allow-origin', 'Content-Type'])
+@cross_origin(origins='*', headers=['access-control-allow-origin', 'Content-Type'])
 def add_profile_record():
     profile_obj = request.json
     print(profile_obj)
@@ -58,13 +72,13 @@ def add_profile_record():
     return "success"
 
 @app.route('/profiles/delete/<profile_id>', methods=['POST'])
-@cross_origin(origin='*', headers=['access-control-allow-origin', 'Content-Type'])
+@cross_origin(origins='*', headers=['access-control-allow-origin', 'Content-Type'])
 def delete_profile_record(profile_id):
     db.profiles.delete_one({'_id': ObjectId(profile_id)})
     return "success"
 
 @app.route('/profiles/update/<profile_id>', methods=['POST'])
-@cross_origin(origin='*', headers=['access-control-allow-origin', 'Content-Type'])
+@cross_origin(origins='*', headers=['access-control-allow-origin', 'Content-Type'])
 def update_profile_record(profile_id):
     profile_obj = request.json
     print(f"{profile_obj}")
