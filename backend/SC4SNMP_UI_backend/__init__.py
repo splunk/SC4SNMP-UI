6befac7f209cc1a445392336cef115f8056a1d94
Wojciech Zyla <wzya@splunk.com>
2022-10-11 11:39:51 +0200

    fix: converting profiles between two formats

--- backend/SC4SNMP_UI_backend/__init__.py
+++ backend/SC4SNMP_UI_backend/__init__.py
@@ -9,8 +9,8 @@ except:
     pass
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
-client = MongoClient('localhost', 27017)
-db = client.sc4snmp
+mongo_client = MongoClient('localhost', 27017)
+db = mongo_client.sc4snmp
 
 
 def create_app():
--- backend/SC4SNMP_UI_backend/common/conversions.py
+++ backend/SC4SNMP_UI_backend/common/conversions.py
@@ -1,66 +1,99 @@
 from abc import abstractmethod
-from collection_manager import ConnectorCollectionManager
-
 
 class Conversion:
-    def __init__(self, mongo, collection_name):
-        self.mongo = mongo
-        self.connector_manager = ConnectorCollectionManager(mongo, collection_name)
 
     @abstractmethod
-    def ui2backend_map(self, ui_profiles, backend_profiles):
+    def _ui2backend_map(self, document: dict):
         pass
 
     @abstractmethod
-    def backend2ui_map(self, ui_profiles, backend_profiles):
+    def _backend2ui_map(self, document: dict):
         pass
 
-    def backend2ui(self):
-        pass
+    def backend2ui(self, document: dict):
+        return self._backend2ui_map(document)
 
-    def ui2backend(self):
-        ui_profiles = self.mongo.sc4snmp.profiles_ui.find({}, {"_id": 0})
-        backend_profiles = self.connector_manager.return_collection()
-        backend_profiles = self.ui2backend_map(ui_profiles, backend_profiles)
-        self.connector_manager.update_collection(backend_profiles)
+    def ui2backend(self, document: dict):
+        return self._ui2backend_map(document)
 
 
-class ProfilesConversion(Conversion):
-    def __init__(self, mongo):
-        super().__init__(mongo, "profiles")
+class ProfileConversion(Conversion):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-    def backend2ui_map(self):
-        pass
+    def _backend2ui_map(self, profile: dict):
+        profile_name = None
+        for key in profile.keys():
+            if key != "_id":
+                profile_name = key
+        if profile_name is None:
+            raise ValueError("No profile name detected")
+        else:
+            backend_var_binds = profile[profile_name]["varBinds"]
+            var_binds = []
+            for vb in backend_var_binds:
+                new_vb = {
+                    "family": vb[0],
+                    "category": vb[1] if len(vb) >= 2 else "",
+                    "index": vb[2] if len(vb) == 3 else "",
+                }
+                var_binds.append(new_vb)
 
-    def ui2backend_map(self, ui_profiles, backend_profiles):
-        for profile in ui_profiles:
-            if profile['conditions']['condition'] == "field":
+            if "condition" in profile[profile_name]:
+                backend_condition = profile[profile_name]["condition"]
+                condition_type = backend_condition["type"]
+                field = backend_condition["field"] if condition_type == "field" else ""
+                patterns = [{"pattern": p} for p in backend_condition["field"]] if condition_type == "field" else None
                 conditions = {
-                    'type': 'field',
-                    'field': profile['conditions']['field'],
-                    'patterns': [el['pattern'] for el in profile['conditions']['patterns']]
+                    "condition": condition_type,
+                    "field": field,
+                    "patterns": patterns
                 }
             else:
                 conditions = {
-                    'type': profile['conditions']['condition']
+                    "condition": "mandatory",
+                    "field": "",
+                    "patterns": None
                 }
-            var_binds = []
-            for var_b in profile['varBinds']:
-                single_var_bind = [var_b['family']]
-                if len(var_b['category']) > 0:
-                    single_var_bind.append(var_b['category'])
-                    if len(var_b['index']) > 0:
-                        single_var_bind.append(int(var_b['index']))
-                var_binds.append(single_var_bind)
-
-            item = {
-                profile['profileName']: {
-                    'frequency': int(profile['frequency']),
-                    'condition': conditions,
-                    'varBinds': var_binds
-                }
+            converted = {
+                "_id": profile["_id"],
+                "profileName": profile_name,
+                "frequency": profile[profile_name]["frequency"],
+                "conditions": conditions,
+                "varBinds": var_binds
             }
-            backend_profiles.update(item)
+            print(converted)
+            return converted
+
+    def _ui2backend_map(self, profile: dict):
+        if profile['conditions']['condition'] == "field":
+            conditions = {
+                'type': 'field',
+                'field': profile['conditions']['field'],
+                'patterns': [el['pattern'] for el in profile['conditions']['patterns']]
+            }
+        else:
+            conditions = {
+                'type': profile['conditions']['condition']
+            }
+        var_binds = []
+        for var_b in profile['varBinds']:
+            single_var_bind = [var_b['family']]
+            if len(var_b['category']) > 0:
+                single_var_bind.append(var_b['category'])
+                if len(var_b['index']) > 0:
+                    single_var_bind.append(int(var_b['index']))
+            var_binds.append(single_var_bind)
+
+        item = {
+            "_id": profile["_id"],
+            profile['profileName']: {
+                'frequency': int(profile['frequency']),
+                'condition': conditions,
+                'varBinds': var_binds
+            }
+        }
+        return item
 
 
 class InventoryConversion(Conversion):
@@ -69,6 +102,6 @@ class InventoryConversion(Conversion):
 
 
 if __name__ == '__main__':
-    from SC4SNMP_UI_backend import client
-    prof_converter = ProfilesConversion(client)
+    from SC4SNMP_UI_backend import mongo_client
+    prof_converter = ProfileConversion(mongo_client)
     prof_converter.ui2backend()
