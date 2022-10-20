cf9f4827c9b6b422e18495b0cbce0cf648adca39
Wojciech Zyla <wzya@splunk.com>
2022-10-20 16:57:14 +0200

    fix: changed the way of accesing colection in endpoints, changed how updates of profiles are made, created check_if_inventory_can_be_added, changed the way of deleting group from inventory when the group was deleted

--- backend/SC4SNMP_UI_backend/ui_handling/routes.py
+++ backend/SC4SNMP_UI_backend/ui_handling/routes.py
@@ -5,6 +5,7 @@ from SC4SNMP_UI_backend import mongo_client
 from SC4SNMP_UI_backend.common.conversions import ProfileConversion, GroupConversion, GroupDeviceConversion, \
     InventoryConversion, get_group_name_from_backend
 from copy import copy
+from enum import Enum
 
 ui = Blueprint('ui', __name__)
 
@@ -12,12 +13,19 @@ profile_conversion = ProfileConversion()
 group_conversion = GroupConversion()
 group_device_conversion = GroupDeviceConversion()
 inventory_conversion = InventoryConversion()
+mongo_profiles = mongo_client.sc4snmp.profiles_ui
+mongo_groups = mongo_client.sc4snmp.groups_ui
+mongo_inventory = mongo_client.sc4snmp.inventory_ui
+
+class InventoryAddEdit(Enum):
+    ADD = 1
+    EDIT = 2
 
 
 @ui.route('/profiles/names')
 @cross_origin()
 def get_profile_names():
-    profiles = mongo_client.sc4snmp.profiles_ui.find()
+    profiles = mongo_profiles.find()
     profiles_list = []
     for pr in list(profiles):
         converted = profile_conversion.backend2ui(pr)
@@ -29,7 +37,7 @@ def get_profile_names():
 @ui.route('/profiles')
 @cross_origin()
 def get_all_profiles_list():
-    profiles = mongo_client.sc4snmp.profiles_ui.find()
+    profiles = mongo_profiles.find()
     profiles_list = []
     for pr in list(profiles):
         converted = profile_conversion.backend2ui(pr)
@@ -43,14 +51,14 @@ def get_all_profiles_list():
 def add_profile_record():
     profile_obj = request.json
     profile_obj = profile_conversion.ui2backend(profile_obj)
-    mongo_client.sc4snmp.profiles_ui.insert_one(profile_obj)
+    mongo_profiles.insert_one(profile_obj)
     return "success"
 
 
 @ui.route('/profiles/delete/<profile_id>', methods=['POST'])
 @cross_origin()
 def delete_profile_record(profile_id):
-    mongo_client.sc4snmp.profiles_ui.delete_one({'_id': ObjectId(profile_id)})
+    mongo_profiles.delete_one({'_id': ObjectId(profile_id)})
     return "success"
 
 
@@ -58,16 +66,25 @@ def delete_profile_record(profile_id):
 @cross_origin()
 def update_profile_record(profile_id):
     profile_obj = request.json
+    new_profile_name = profile_obj['profileName']
     profile_obj = profile_conversion.ui2backend(profile_obj)
-    new_values = {"$set": profile_obj}
-    mongo_client.sc4snmp.profiles_ui.update_one({'_id': ObjectId(profile_id)}, new_values)
+
+    old_profile = list(mongo_profiles.find({'_id': ObjectId(profile_id)}, {"_id": 0}))[0]
+    old_profile_name = list(old_profile.keys())[0]
+
+    # TODO: if profile name was changed, update all inventory records which used this profile
+    if old_profile_name != new_profile_name:
+        mongo_profiles.update_one({'_id': ObjectId(profile_id)},
+                                  {"$rename": {f"{old_profile_name}": f"{new_profile_name}"}})
+    mongo_profiles.update_one({'_id': ObjectId(profile_id)},
+                              {"$set": {new_profile_name: profile_obj[new_profile_name]}})
     return "success"
 
 
 @ui.route('/groups')
 @cross_origin()
 def get_groups_list():
-    groups = mongo_client.sc4snmp.groups_ui.find()
+    groups = mongo_groups.find()
     groups_list = []
     for gr in list(groups):
         groups_list.append(group_conversion.backend2ui(gr))
@@ -79,7 +96,7 @@ def get_groups_list():
 def add_group_record():
     group_obj = request.json
     group_obj = group_conversion.ui2backend(group_obj)
-    mongo_client.sc4snmp.groups_ui.insert_one(group_obj)
+    mongo_groups.insert_one(group_obj)
     return "success"
 
 
@@ -87,34 +104,36 @@ def add_group_record():
 @cross_origin()
 def update_group(group_id):
     group_obj = request.json
-    old_group = mongo_client.sc4snmp.groups_ui.find({'_id': ObjectId(group_id)})
+    old_group = mongo_groups.find({'_id': ObjectId(group_id)})
     old_group = list(old_group)[0]
     old_group_name = get_group_name_from_backend(old_group)
-    mongo_client.sc4snmp.groups_ui.update_one({'_id': old_group['_id']}, {"$rename": {f"{old_group_name}": f"{group_obj['groupName']}"}})
+    mongo_groups.update_one({'_id': old_group['_id']}, {"$rename": {f"{old_group_name}": f"{group_obj['groupName']}"}})
 
-    # TODO: Inform user about changes in the inventory
     # rename corresponding group in inventory
-    mongo_client.sc4snmp.inventory_ui.update_one({"address": old_group_name}, {"$set": {"address": group_obj['groupName']}})
-    return "success"
+    mongo_inventory.update_one({"address": old_group_name}, {"$set": {"address": group_obj['groupName']}})
+    return json_util.dumps({"message": f"{old_group_name} was also renamed to {group_obj['groupName']} in the inventory"}), 200
 
 
 @ui.route('/groups/delete/<group_id>', methods=['POST'])
 @cross_origin()
 def delete_group_and_devices(group_id):
-    group = mongo_client.sc4snmp.groups_ui.find({'_id': ObjectId(group_id)})
+    group = mongo_groups.find({'_id': ObjectId(group_id)})
     group = list(group)[0]
     group_name = get_group_name_from_backend(group)
     with mongo_client.start_session() as session:
         with session.start_transaction():
-            mongo_client.sc4snmp.groups_ui.delete_one({'_id': ObjectId(group_id)})
-            mongo_client.sc4snmp.inventory_ui.update_one({"address": group_name}, {"$set": {"delete": True}})
+            mongo_groups.delete_one({'_id': ObjectId(group_id)})
+            existing_inventory = list(mongo_inventory.find({"address": group_name, "delete": False}))
+            if len(existing_inventory) > 0:
+                # TODO: It is not working from the sc4snmp side
+                mongo_inventory.update_one({"address": group_name}, {"$set": {"delete": True}})
     return json_util.dumps({"message": f"{group_name} was also deleted from inventory"}), 200
 
 
 @ui.route('/group/<group_id>/devices/count')
 @cross_origin()
 def get_devices_count_for_group(group_id):
-    group = mongo_client.sc4snmp.groups_ui.find({"_id": ObjectId(group_id)})
+    group = mongo_groups.find({"_id": ObjectId(group_id)})
     group = list(group)[0]
     group_name = get_group_name_from_backend(group)
     total_count = len(group[group_name])
@@ -127,7 +146,7 @@ def get_devices_of_group(group_id, page_num, dev_per_page):
     page_num = int(page_num)
     dev_per_page = int(dev_per_page)
     skips = dev_per_page * (page_num - 1)
-    group = mongo_client.sc4snmp.groups_ui.find({"_id": ObjectId(group_id)})
+    group = mongo_groups.find({"_id": ObjectId(group_id)})
     group = list(group)[0]
     try:
         group_name = get_group_name_from_backend(group)
@@ -148,15 +167,14 @@ def get_devices_of_group(group_id, page_num, dev_per_page):
 def add_device_to_group():
     device_obj = request.json
     group_id = device_obj["groupId"]
-    group = mongo_client.sc4snmp.groups_ui.find({'_id': ObjectId(group_id)}, {"_id": 0})
+    group = mongo_groups.find({'_id': ObjectId(group_id)}, {"_id": 0})
     group = list(group)[0]
     device_obj = group_device_conversion.ui2backend(device_obj)
 
     group_name = get_group_name_from_backend(group)
     group[group_name].append(device_obj)
     new_values = {"$set": group}
-    print(group_id, new_values)
-    mongo_client.sc4snmp.groups_ui.update_one({"_id": ObjectId(group_id)}, new_values)
+    mongo_groups.update_one({"_id": ObjectId(group_id)}, new_values)
     return "success"
 
 
@@ -166,14 +184,14 @@ def update_device_from_group(device_id):
     device_obj = request.json
     group_id = device_id.split("-")[0]
     device_id = device_id.split("-")[1]
-    group = mongo_client.sc4snmp.groups_ui.find({'_id': ObjectId(group_id)}, {"_id": 0})
+    group = mongo_groups.find({'_id': ObjectId(group_id)}, {"_id": 0})
     group = list(group)[0]
     device_obj = group_device_conversion.ui2backend(device_obj)
 
     group_name = get_group_name_from_backend(group)
     group[group_name][int(device_id)] = device_obj
     new_values = {"$set": group}
-    mongo_client.sc4snmp.groups_ui.update_one({"_id": ObjectId(group_id)}, new_values)
+    mongo_groups.update_one({"_id": ObjectId(group_id)}, new_values)
     return "success"
 
 
@@ -182,12 +200,12 @@ def update_device_from_group(device_id):
 def delete_device_from_group_record(device_id: str):
     group_id = device_id.split("-")[0]
     device_id = device_id.split("-")[1]
-    group = mongo_client.sc4snmp.groups_ui.find({'_id': ObjectId(group_id)}, {"_id": 0})
+    group = mongo_groups.find({'_id': ObjectId(group_id)}, {"_id": 0})
     group = list(group)[0]
     group_name = get_group_name_from_backend(group)
     group[group_name].pop(int(device_id))
     new_values = {"$set": group}
-    mongo_client.sc4snmp.groups_ui.update_one({"_id": ObjectId(group_id)}, new_values)
+    mongo_groups.update_one({"_id": ObjectId(group_id)}, new_values)
     return "success"
 
 
@@ -199,7 +217,7 @@ def get_inventory_list(page_num, dev_per_page):
     dev_per_page = int(dev_per_page)
     skips = dev_per_page * (page_num - 1)
 
-    inventory = mongo_client.sc4snmp.inventory_ui.find({"delete": False}).skip(skips).limit(dev_per_page)
+    inventory = mongo_inventory.find({"delete": False}).skip(skips).limit(dev_per_page)
     inventory_list = []
     for inv in list(inventory):
         inventory_list.append(inventory_conversion.backend2ui(inv))
@@ -209,8 +227,12 @@ def get_inventory_list(page_num, dev_per_page):
 @ui.route('/inventory/count')
 @cross_origin()
 def get_inventory_count():
-    total_count = mongo_client.sc4snmp.inventory_ui.count_documents({"delete": False})
+    total_count = mongo_inventory.count_documents({"delete": False})
     return json_util.dumps(total_count)
+
+# TODO: In the inventory_ui there should be only one document for the specific record. If user adds a record,
+#  then removes it and then adds it once again, there will be three documents in the inventory_ui with different
+#  delete flags. -> Done, but requires more testing
 
 
 @ui.route('/inventory/add', methods=['POST'])
@@ -218,31 +240,13 @@ def get_inventory_count():
 def add_inventory_record():
     inventory_obj = request.json
     inventory_obj = inventory_conversion.ui2backend(inventory_obj, delete=False)
-    address = inventory_obj['address']
-    port = inventory_obj['port']
-
-    if address[0].isdigit():
-        inventory_record = mongo_client.sc4snmp.inventory_ui.find({'address': address, 'port': port, "delete": False})
-        identifier = f"{address}:{port}"
-    else:
-        inventory_record = mongo_client.sc4snmp.inventory_ui.find({'address': address, "delete": False})
-        identifier = address
-        group = list(mongo_client.sc4snmp.groups_ui.find({address: {"$exists": 1}}))
-        if len(group) == 0:
-            return json_util.dumps({"message": f"There is no group {address} configured. Record was not added."}), 400
-
-    if len(list(inventory_record)) == 0:
-        mongo_client.sc4snmp.inventory_ui.insert_one(inventory_obj)
-        return "success"
-    else:
-        return json_util.dumps({"message": f"Inventory record for {identifier} already exists. Record was not added."}), 400
-
+    return check_if_inventory_can_be_added(inventory_obj, InventoryAddEdit.ADD, None)
 
 
 @ui.route('/inventory/delete/<inventory_id>', methods=['POST'])
 @cross_origin()
 def delete_inventory_record(inventory_id):
-    mongo_client.sc4snmp.inventory_ui.update_one({"_id": ObjectId(inventory_id)}, {"$set": {"delete": True}})
+    mongo_inventory.update_one({"_id": ObjectId(inventory_id)}, {"$set": {"delete": True}})
     return "success"
 
 
@@ -251,5 +255,53 @@ def delete_inventory_record(inventory_id):
 def update_inventory_record(inventory_id):
     inventory_obj = request.json
     inventory_obj = inventory_conversion.ui2backend(inventory_obj, delete=False)
-    mongo_client.sc4snmp.inventory_ui.update_one({"_id": ObjectId(inventory_id)}, {"$set": inventory_obj})
-    return "success"
+    return check_if_inventory_can_be_added(inventory_obj, InventoryAddEdit.EDIT, inventory_id)
+
+
+def check_if_inventory_can_be_added(inventory_obj, change_type: InventoryAddEdit, inventory_id):
+    address = inventory_obj['address']
+    port = inventory_obj['port']
+    message = "added" if change_type == InventoryAddEdit.ADD else "edited"
+    inventory_id = ObjectId(inventory_id) if change_type == InventoryAddEdit.EDIT else None
+
+    check_duplicates = False
+    if address[0].isdigit():
+        # record is a single host
+        existing_inventory_record = list(mongo_inventory.find({'address': address, 'port': port, "delete": False}))
+        deleted_inventory_record = list(mongo_inventory.find({'address': address, 'port': port, "delete": True}))
+        identifier = f"{address}:{port}"
+        check_duplicates = True
+    else:
+        # record is a group
+        existing_inventory_record = list(mongo_inventory.find({'address': address, "delete": False}))
+        deleted_inventory_record = list(mongo_inventory.find({'address': address, "delete": True}))
+        identifier = address
+        group = list(mongo_groups.find({address: {"$exists": 1}}))
+        if len(group) == 0:
+            result = json_util.dumps({"message": f"There is no group {address} configured. Record was not {message}."}), 400
+        else:
+            check_duplicates = True
+
+    if check_duplicates:
+        # check if the same record already exist in the inventory
+        if len(existing_inventory_record) == 0:
+            make_change = True
+        elif existing_inventory_record[0]["_id"] == inventory_id and change_type == InventoryAddEdit.EDIT:
+            make_change = True
+        else:
+            make_change = False
+
+        if make_change:
+            if change_type == InventoryAddEdit.ADD:
+                mongo_inventory.insert_one(inventory_obj)
+            else:
+                mongo_inventory.update_one({"_id": inventory_id}, {"$set": inventory_obj})
+
+            if len(deleted_inventory_record) > 0:
+                mongo_inventory.delete_one({"_id": deleted_inventory_record[0]["_id"]})
+            result = "success", 200
+        else:
+            result = json_util.dumps(
+                {"message": f"Inventory record for {identifier} already exists. Record was not {message}."}), 400
+
+    return result
