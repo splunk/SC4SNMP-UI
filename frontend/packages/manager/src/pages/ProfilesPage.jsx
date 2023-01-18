04fff13042230690156431df6097451ba2a44700
Wojciech Zyla <wzya@splunk.com>
2023-01-18 15:01:33 +0100

    fix: update form in profiles

--- frontend/packages/manager/src/pages/ProfilesPage.jsx
+++ frontend/packages/manager/src/pages/ProfilesPage.jsx
@@ -1,6 +1,5 @@
-import React, {useContext} from 'react';
-import ProfilePanel from "../components/profiles/ProfilePanel"
-import AddProfileModal from "../components/profiles/AddProfileModal"
+import React from 'react';
+import AddProfileModal from "../components/profiles/AddProfileModal";
 import ProfilesList from "../components/profiles/ProfilesList";
 
 function ProfilesPage(){
