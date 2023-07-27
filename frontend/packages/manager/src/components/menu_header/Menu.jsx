4bf090f5a593c824908638852cde44a5fd236f7b
Wojciech Zyla <wzya@splunk.com>
2023-07-27 15:18:48 +0200

    fix: stay on the same page after refreshing the browser

--- frontend/packages/manager/src/components/menu_header/Menu.jsx
+++ frontend/packages/manager/src/components/menu_header/Menu.jsx
@@ -1,15 +1,31 @@
-import React, {useCallback} from 'react';
-import {useMenuHeaderContxt} from '../../store/menu-header-contxt';
-import { StyledTab, StyledMenuBar, StyledMenuBarLeft, StyledMenuBarRight } from "../../styles/menu_header/MenuBarStyle";
+import React, {useCallback, useEffect} from 'react';
 import P from '@splunk/react-ui/Paragraph';
 import TabBar from '@splunk/react-ui/TabBar';
+import { createBrowserHistory } from "history"
+import qs from "qs";
+import {useMenuHeaderContxt} from '../../store/menu-header-contxt';
+import { StyledTab, StyledMenuBar, StyledMenuBarLeft, StyledMenuBarRight } from "../../styles/menu_header/MenuBarStyle";
 
 function Menu(){
     const MenuCtx = useMenuHeaderContxt();
 
+    const history = createBrowserHistory();
+
     const handleMenuChange = useCallback((e, { selectedTabId }) => {
         MenuCtx.setActiveTabId(selectedTabId);
     }, []);
+
+    useEffect(() => {
+        const filterParams = history.location.search.substring(1);
+        const filtersFromParams = qs.parse(filterParams);
+        if (filtersFromParams.tab) {
+            MenuCtx.setActiveTabId(filtersFromParams.tab);
+        }
+      }, []);
+
+      useEffect(() => {
+        history.push(`?tab=${MenuCtx.activeTabId}`);
+      }, [MenuCtx.activeTabId]);
 
     return(
             <StyledMenuBar>
