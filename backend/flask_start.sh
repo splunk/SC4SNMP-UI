bc025f3510c782212c4f15051df87974b84fd5a1
omrozowicz-splunk <omrozowicz@splunk.com>
2025-11-18 10:53:22 +0100

    fix: get rid off loggers

--- backend/flask_start.sh
+++ backend/flask_start.sh
@@ -4,10 +4,6 @@ set -e
 # Source Redis URL construction
 . /app/construct-redis-url.sh
 
-echo "Starting Gunicorn with:"
-echo "  REDIS_URL=${REDIS_URL}"
-echo "  CELERY_BROKER_URL=${CELERY_BROKER_URL}"
-
 set -o errexit
 set -o nounset
 
