e748406adea4d4712e0a87d828d99c8e02eb8ab8
srv-rr-github-token <94607705+srv-rr-github-token@users.noreply.github.com>
2026-07-30 14:02:07 +0000

    chore(release): 1.2.3-beta.5
    ## [1.2.3-beta.5](https://github.com/splunk/SC4SNMP-UI/compare/v1.2.3-beta.4...v1.2.3-beta.5) (2026-07-30)
    
    ### Bug Fixes
    
    * load config from files ([#144](https://github.com/splunk/SC4SNMP-UI/issues/144)) ([afabf12](https://github.com/splunk/SC4SNMP-UI/commit/afabf12c3a36c42fd563645bd2c8be0234dcdb7a))

--- backend/SC4SNMP_UI_backend/__init__.py
+++ backend/SC4SNMP_UI_backend/__init__.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-__version__ = "1.2.3-beta.4"
+__version__ = "1.2.3-beta.5"
 
 MONGO_URI = os.getenv("MONGO_URI")
 log = logging.getLogger('gunicorn.error')
