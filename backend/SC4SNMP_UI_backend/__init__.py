da1a6d55d558eb0d8cf66cf6dd98dfc47972647c
srv-rr-github-token <94607705+srv-rr-github-token@users.noreply.github.com>
2026-08-17 07:41:33 +0000

    chore(release): 1.2.3-beta.7
    ## [1.2.3-beta.7](https://github.com/splunk/SC4SNMP-UI/compare/v1.2.3-beta.6...v1.2.3-beta.7) (2026-08-17)
    
    ### Bug Fixes
    
    * add pagination for groups ([#148](https://github.com/splunk/SC4SNMP-UI/issues/148)) ([89f68a1](https://github.com/splunk/SC4SNMP-UI/commit/89f68a18ea62b202f6d7f27aa5c5db6b4901dd51))

--- backend/SC4SNMP_UI_backend/__init__.py
+++ backend/SC4SNMP_UI_backend/__init__.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-__version__ = "1.2.3-beta.6"
+__version__ = "1.2.3-beta.7"
 
 MONGO_URI = os.getenv("MONGO_URI")
 log = logging.getLogger('gunicorn.error')
